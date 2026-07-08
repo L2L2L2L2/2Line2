933a1d26b5e0d033a93648b5b984ce92ec65d7f8
L2L2L2L2 <pkt@ictr.or.kr>
2026-07-08 23:13:02 +0900

    Update data.js

--- data.js
+++ data.js
@@ -2,243 +2,291 @@
 // 총 48개의 장소 데이터가 포함되어 있습니다.
 const placeData = {
     "211ES001": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 1호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE08.jpg",
-        "additional_data": "추가적인 안내문구를 작성하실 수 있습니다." // 이 내용이 화면에 뜸
+        "additional_data": "추가적인 안내문구를 작성하실 수 있습니다."
     },
     "211ES002": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 2호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE14.jpg",
-        "additional_data": "추가적인 안내문구를 작성하실 수 있습니다." // 이 내용이 화면에 뜸
+        "additional_data": "현재 이 에스컬레이터는 하행 전용으로 운행 중입니다."
     },
     "211ES003": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 3호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE07.jpg"
     },
     "211ES004": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 4호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE12.jpg"
     },
     "211ES005": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 5호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE07.jpg"
     },
     "211ES006": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 6호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE09.jpg"
     },
     "211ES007": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 7호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE08.jpg"
     },
     "211ES008": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 8호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE11.jpg"
     },
     "211ES009": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 9호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE08.jpg"
     },
     "211ES010": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 10호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE08.jpg"
     },
     "211ES011": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 11호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE07.jpg"
     },
     "211ES012": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 12호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE07.jpg"
     },
     "211ES013": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 13호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE02.jpg"
     },
     "211ES014": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 14호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE02.jpg"
     },
     "211ES015": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 15호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE05.jpg"
     },
     "211ES016": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 16호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE05.jpg"
     },
     "211ES017": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 17호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE01.jpg"
     },
     "211ES018": {
+        "station": "가정역",
         "title": "가정역 에스컬레이터 18호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE01.jpg"
     },
     "211EV001": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 1호기(B1)",
         "phone": "032-451-4311",
         "image_name": "211ZONE02.jpg"
     },
     "211EV002": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 2호기(B1)",
         "phone": "032-451-4311",
         "image_name": "211ZONE05.jpg"
     },
     "211EV003": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 3호기(B1)",
         "phone": "032-451-4311",
         "image_name": "211ZONE05.jpg"
     },
     "211EV004": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 4호기(B1)",
         "phone": "032-451-4311",
         "image_name": "211ZONE02.jpg"
     },
     "211EV005": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 5호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE02.jpg"
     },
     "211EV006": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 6호기",
         "phone": "032-451-4311",
         "image_name": "211ZONE02.jpg"
     },
     "211EV007": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 1호기(B3)",
         "phone": "032-451-4311",
         "image_name": "211ZONE13.jpg"
     },
     "211EV008": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 2호기(B3)",
         "phone": "032-451-4311",
         "image_name": "211ZONE13.jpg"
     },
     "211EV009": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 3호기(B3)",
         "phone": "032-451-4311",
         "image_name": "211ZONE10.jpg"
     },
     "211EV010": {
+        "station": "가정역",
         "title": "가정역 엘리베이터 4호기(B3)",
         "phone": "032-451-4311",
         "image_name": "211ZONE10.jpg"
     },
     "211EXIT001": {
+        "station": "가정역",
         "title": "가정역 1번출구",
         "phone": "032-451-4311",
         "image_name": "211ZONE03.jpg"
     },
     "211EXIT002": {
+        "station": "가정역",
         "title": "가정역 2번출구",
         "phone": "032-451-4311",
         "image_name": "211ZONE06.jpg"
     },
     "211EXIT003": {
+        "station": "가정역",
         "title": "가정역 3번출구",
         "phone": "032-451-4311",
         "image_name": "211ZONE06.jpg"
     },
     "211EXIT004": {
+        "station": "가정역",
         "title": "가정역 4번출구",
         "phone": "032-451-4311",
         "image_name": "211ZONE04.jpg"
     },
     "211EXIT005": {
+        "station": "가정역",
         "title": "가정역 5번출구",
         "phone": "032-451-4311",
         "image_name": "211ZONE04.jpg"
     },
     "211EXIT006": {
+        "station": "가정역",
         "title": "가정역 6번출구",
         "phone": "032-451-4311",
         "image_name": "211ZONE01.jpg"
     },
     "211EXIT007": {
+        "station": "가정역",
         "title": "가정역 7번출구",
         "phone": "032-451-4311",
         "image_name": "211ZONE01.jpg"
     },
     "211EXIT008": {
+        "station": "가정역",
         "title": "가정역 8번출구",
         "phone": "032-451-4311",
         "image_name": "211ZONE03.jpg"
     },
     "211TOILET6M": {
+        "station": "가정역",
         "title": "가정역 6번출구 방면 남자화장실",
         "phone": "032-451-4311",
         "image_name": "211ZONE01.jpg"
     },
     "211TOILET6W": {
+        "station": "가정역",
         "title": "가정역 6번출구 방면 여자화장실",
         "phone": "032-451-4311",
         "image_name": "211ZONE01.jpg"
     },
     "211TOILET5M": {
+        "station": "가정역",
         "title": "가정역 5번출구 방면 남자화장실",
         "phone": "032-451-4311",
         "image_name": "211ZONE04.jpg"
     },
     "211TOILET5W": {
+        "station": "가정역",
         "title": "가정역 5번출구 방면 여자화장실",
         "phone": "032-451-4311",
         "image_name": "211ZONE04.jpg"
     },
     "211UPFRONT": {
+        "station": "가정역",
         "title": "가정역 상선 전부(검단오류 방면)",
         "phone": "032-451-4311",
         "image_name": "211ZONE14.jpg"
     },
     "211UPMID": {
+        "station": "가정역",
         "title": "가정역 상선 중앙(검단오류 방면)",
         "phone": "032-451-4311",
         "image_name": "211ZONE13.jpg"
     },
     "211UPREAR": {
+        "station": "가정역",
         "title": "가정역 상선 후부(검단오류 방면)",
         "phone": "032-451-4311",
         "image_name": "211ZONE12.jpg"
     },
     "211DOWNFRONT": {
+        "station": "가정역",
         "title": "가정역 하선 전방(운연 방면)",
         "phone": "032-451-4311",
         "image_name": "211ZONE09.jpg"
     },
     "211DOWNMID": {
+        "station": "가정역",
         "title": "가정역 하선 중앙(운연 방면)",
         "phone": "032-451-4311",
         "image_name": "211ZONE10.jpg"
     },
     "211DOWNREAR": {
+        "station": "가정역",
         "title": "가정역 하선 후부(운연 방면)",
         "phone": "032-451-4311",
         "image_name": "211ZONE11.jpg"
     },
     "211STA001": {
+        "station": "가정역",
         "title": "가정역 대합실(1, 6, 7, 8번 출구 방면)",
         "phone": "032-451-4311",
         "image_name": "211ZONE02.jpg"
     },
     "211STA002": {
+        "station": "가정역",
         "title": "가정역 대합실(2, 3, 4, 5번 출구 방면)",
         "phone": "032-451-4311",
         "image_name": "211ZONE05.jpg"
